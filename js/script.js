e3fc18f12d739e2f6b92fa7e5963fb495ebfa8d6
Adetunji Adeleye <igbengoos@hotmail.com>
2025-01-30 20:24:46 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-function formatTime(date) {
-    var hours   = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours()
-    var minutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes()
-
-    return hours + ':' + minutes
-}
+let a;
+let time;
+let date;
+const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
+setInterval(() => {
+    a = new Date();
+    date = a.toLocaleDateString(undefined, options);
+    time = a.getHours() + ":" + a.getMinutes();
+    document.getElementById("time").innerHTML = time;
+}, 1000);
